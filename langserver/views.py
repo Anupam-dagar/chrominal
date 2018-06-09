f3bc36d74ea4504b3d8702bf8d8fd853a1bfb62a
Anupam-dagar <dagaranupam@gmail.com>
2018-06-10 03:33:20 +0530

    compilation: Add C compiler view.
    On 2 consecutive compilation with different codes, same output comes,need to fix.

--- langserver/views.py
+++ langserver/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import os
+from subprocess import call, Popen, PIPE
+import json
 # Create your views here.
+@csrf_exempt
+def compilecode(request):
+    import pdb; pdb.set_trace()
+    with open('ccode.c','w+') as mycode:
+        mycode.write(request.POST.get('code'))
+    Popen(["gcc","ccode.c"])
+    myvar = Popen(["./a.out"], stdout=PIPE)
+    output = myvar.communicate()[0]
+    print(output)
+    Popen(["rm","ccode.c"])
+    return JsonResponse({"success": output.decode()})
